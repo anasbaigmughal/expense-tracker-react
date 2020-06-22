b925be649079f6e15284569a729ac6bbd19d5351
Daniya Sehrish <anasbaigmughal@gmail.com>
2020-06-22 19:18:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Learning React Native
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
