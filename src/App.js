fa1e1e40e9db7f040a3b8ea693dac3329feb9e6f
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-07-20 19:02:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
 
   );
