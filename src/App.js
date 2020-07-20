ee733a471d0d5ea9d98af1c4b3a0e9daf36c8f9f
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-07-20 09:56:07 +0500

    creating app from scratch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    
-    <div className="App">
-
-      <header className="App-header">
-        Learning React Native
-        <img src={logo} className="App-logo" alt="logo" />
-      
-      </header>
+    <div>
+      Hello World
     </div>
 
   );
