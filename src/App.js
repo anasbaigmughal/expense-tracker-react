35d54fa87abf140f74b42b5861d4eb7d8b49f3c7
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-07-21 13:19:18 +0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Header } from './components/Header'
 import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
 import './App.css';
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Header />
       <div className='container'>
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
 
